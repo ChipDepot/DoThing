1c74fa4117ff9c6cae62592ff7d83084d011fd27
dnl <danieldavid2001@gmail.com>
2024-01-12 14:31:03 -0500

    testing things

--- dothing/src/main.rs
+++ dothing/src/main.rs
@@ -1,5 +1,57 @@
-use starduck::Application;
+use docker_api::{
+    opts::{ContainerConnectionOpts, ContainerCreateOpts, ContainerListOpts},
+    Container, Docker,
+};
 
-fn main() {
+use starduck::Doctrine;
+
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+
+    if let Ok(docker) = Docker::new("unix:///run/docker.sock") {
+        let _info = docker.info().await.unwrap();
+
+        let opts = ContainerListOpts::builder().all(true).build();
+
+        let containers = docker.containers();
+
+        let env_vars = vec!["RUST_LOG=mocker", "ip=mqtt_broker"];
+        let cmd = vec!["topic:co2"];
+
+        let create_opts = ContainerCreateOpts::builder()
+            .image("mrdahaniel/mocker")
+            .name("MokerTest")
+            .env(env_vars)
+            .command(cmd)
+            .build();
+
+        let res = containers.create(&create_opts).await.unwrap();
+        let inspect_res = res.inspect().await.unwrap();
+
+        res.start().await.unwrap();
+
+        println!("{}", serde_json::to_string_pretty(&inspect_res).unwrap());
+
+        let container_id = res.id();
+
+        let network_opts = ContainerConnectionOpts::builder(container_id).build();
+
+        if let Err(e) = docker
+            .networks()
+            .get("smart_campus_production_smart_uis")
+            .connect(&network_opts)
+            .await
+        {
+            println!("Err: {}", e);
+        };
+
+        docker
+            .containers()
+            .get(container_id.clone())
+            .start()
+            .await
+            .unwrap();
+        // println!("{:?}", containers.list(&opts).await.unwrap());
+    };
 }
